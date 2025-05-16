71d738b805c5ff21f2213d9c496541f46a4fc977
Vicente Soriano <vicente.soriano@broadcom.com>
2025-05-16 16:54:55 +0200

    first commit

---
+++ src/main/java/com/example/demo_2_grim_fandango/Demo2GrimFandangoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_2_grim_fandango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2GrimFandangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2GrimFandangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_2_grim_fandango/Demo2GrimFandangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_2_grim_fandango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2GrimFandangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
